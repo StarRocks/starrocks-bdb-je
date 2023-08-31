995d25b458231ab06dd1771be419ff0963a39413
gengjun-git <gengjun@starrocks.com>
2023-08-31 12:31:33 +0800

    opt
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- build.sh
+++ build.sh
@@ -1,1 +1,39 @@
+if [[ -z ${JAVA_HOME} ]]; then
+    echo "JAVA_HOME is not set"
+    exit 1
+fi
+
+jdk_version() {
+    local result
+    local java_cmd=$JAVA_HOME/bin/java
+    local IFS=$'\n'
+    # remove \r for Cygwin
+    local lines=$("$java_cmd" -Xms32M -Xmx32M -version 2>&1 | tr '\r' '\n')
+    if [[ -z $java_cmd ]]
+    then
+        result=no_java
+    else
+        for line in $lines; do
+            if [[ (-z $result) && ($line = *"version \""*) ]]
+            then
+                local ver=$(echo $line | sed -e 's/.*version "\(.*\)"\(.*\)/\1/; 1q')
+                # on macOS, sed doesn't support '?'
+                if [[ $ver = "1."* ]]
+                then
+                    result=$(echo $ver | sed -e 's/1\.\([0-9]*\)\(.*\)/\1/; 1q')
+                else
+                    result=$(echo $ver | sed -e 's/\([0-9]*\)\(.*\)/\1/; 1q')
+                fi
+            fi
+        done
+    fi
+    echo "$result"
+}
+
+JAVA_VERSION=$(jdk_version)
+if [[ "$JAVA_VERSION" -ne "8" ]]; then
+    echo "must use jdk8 to compile"
+    exit 1
+fi
+
 mvn package
