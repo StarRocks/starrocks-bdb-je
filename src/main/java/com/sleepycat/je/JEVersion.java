e5708c1f58b680511ad3a67e703d2de3864af201
gengjun-git <gengjun@starrocks.com>
2024-08-29 15:18:03 +0800

    Change version to 18.3.19 (#18)
    Signed-off-by: gengjun-git <gengjun@starrocks.com>

--- src/main/java/com/sleepycat/je/JEVersion.java
+++ src/main/java/com/sleepycat/je/JEVersion.java
@@ -31,7 +31,7 @@ public class JEVersion implements Comparable<JEVersion>, Serializable {
      * Release version.
      */
     public static final JEVersion CURRENT_VERSION =
-        new JEVersion(18, 3, 18, null);
+        new JEVersion(18, 3, 19, null);
 
     private final int majorNum;
     private final int minorNum;
